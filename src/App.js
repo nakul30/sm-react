6bfbee04d87a153cecf5924736234bde90842e52
Nakul <nakulgarg1424@gmail.com>
2023-12-18 02:04:09 +0530

    before Navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux'
 import PostsList from './components/PostList';
 import {fetchPosts} from "./state/actions-create/posts"
 import propTypes from 'prop-types'
-import Navbar from './components/Navbar';
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts()) ;   /// to dispatch an action here to put post in loaded component 
@@ -14,7 +13,7 @@ class App extends React.Component {
     const {posts} = this.props
     return (
       <div>
-        <Navbar/>
+
         <PostsList posts={posts}/>
       </div>
     )
