28bd7866b9009c90423ec4713983dff59fc9055d
Nakul <nakulgarg1424@gmail.com>
2023-12-20 02:41:49 +0530

    Private Route

--- src/App.js
+++ src/App.js
@@ -1,63 +1,88 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 import Navbar from './components/Navbar';
-import {fetchPosts} from "./state/actions-create/posts"
+import { fetchPosts } from './state/actions-create/posts';
 import propTypes from 'prop-types';
 import Page404 from './components/Page404';
 // import { useParams, useLocation, useHistory } from 'react-router-dom';
-import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
-// import * as jwtDecode from 'jwt-decode'
+
 import { jwtDecode } from 'jwt-decode';
 
 import Signup from './components/Signup';
 import { authenticateUser } from './state/actions-create/auth';
+// import auth from './state/reducers/auth-reducer';
+ 
+const Settings = () => <div>Settings</div>;
 
-// const Settings = () => <div>Setting </div>
+const PrivateRoute = (privateRouteProps) => {
+  //change location to path we are sending path not location
+  const { isLoggedin, children , path} = privateRouteProps;
+  // console.log("Private route --------------------------> " , privateRouteProps) ; 
+  console.log('element------------------> ', children);
+  console.log('isloggedin--------------->', isLoggedin);
+  // console.log('path------------------>', path);
+  return isLoggedin ? children : <Navigate to="/login" ></Navigate>;
+};
+
 class App extends React.Component {
   componentDidMount() {
-    this.props.dispatch(fetchPosts()) ;   /// to dispatch an action here to put post in loaded component 
-    const token = localStorage.getItem('token') ; 
-    if(token){
-      const user = jwtDecode(token) ; 
-      console.log('user',user);
-      this.props.dispatch(authenticateUser({
-        email: user.email , 
-        _id : user._id , 
-        name : user.name 
-      }))
+    this.props.dispatch(fetchPosts()); /// to dispatch an action here to put post in loaded component
+    const token = localStorage.getItem('token');
+    if (token) {
+      const user = jwtDecode(token);
+      console.log('user', user);
+      this.props.dispatch(
+        authenticateUser({
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+        })
+      );
     }
   }
   render() {
-    console.log('PROPS :', this.props) ; 
-    const {posts} = this.props
+    console.log('PROPS :', this.props);
+    const { posts, auth } = this.props;
     // const { posts, ...restProps } = this.props;
-      return (
+    return (
       <Router>
         <div>
-          <Navbar/>
-         <Routes>
-          
-            <Route exact path="/" element={<Home posts={posts}/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Signup/>} />
-            {/* <Route exact path="/settings" element={ <PrivateRoute isLoggedin = {auth.isLoggedin} path={location.pathname}> <Settings/></PrivateRoute>}/> */}
-            {/* <PrivateRoute path='/settings' element={<Setting/>}></PrivateRoute> */}
-            <Route  path = "*" element ={<Page404 />} />
+          <Navbar />
+          <Routes>
+            
+            <Route exact path="/" element={<Home posts={posts} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Signup />} />
+            <Route
+              path="/settings"
+              element={
+                <PrivateRoute isLoggedin={auth.isLoggedin} > 
+                  <Settings /> 
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<Page404 />} />
           </Routes>
-          
         </div>
       </Router>
-    )
+    );
   }
 }
 // export default App
 const mapStateToProps = (state) => ({
-    posts : state.posts,
-})
-App.propTypes = { 
-  posts : propTypes.array.isRequired
-}
+  posts: state.posts,
+  auth: state.auth,
+});
+App.propTypes = {
+  posts: propTypes.array.isRequired,
+};
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
